b65c4e4d32a71c5d675fb9a20bdd18f9af1fbb60
Sifat Shahriar Khan <sifat.torque@gmail.com>
2023-08-16 17:56:45 -0400

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -38,7 +38,7 @@ def get_csvs_df(path):
 # TO DO: add function to split data
 def split_data(df):
     X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
     return X_train, X_test, y_train, y_test
     
 
