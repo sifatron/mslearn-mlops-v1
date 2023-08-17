42c9dace10629c5bef9a61c5a3ce12c9b87118e8
Sifat Shahriar Khan <sifat.torque@gmail.com>
2023-08-17 10:31:24 -0400

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -40,8 +40,8 @@ def split_data(df):
     X, y = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure', 
                'TricepsThickness', 'SerumInsulin', 'BMI', 
                'DiabetesPedigree', 'Age']].values, df['Diabetic'].values
-    X_train, X_test, y_train, y_test = 
-    train_test_split(X, y, test_size=0.20, random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, 
+                                                        test_size=0.20, random_state=0)
     return X_train, X_test, y_train, y_test
 
 
